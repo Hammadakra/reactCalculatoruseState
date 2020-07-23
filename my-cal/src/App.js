034a1bcf010fa1f69f202f62a7e7152d18395a5f
Hammadakra <41269248+Hammadakra@users.noreply.github.com>
2020-07-23 11:35:30 -0700

    files

--- my-cal/src/App.js
+++ my-cal/src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    
+    <div>
+    <h1>Adding Two Number</h1>
+    <div className="inp">
+    <input type='number' placeholder='0'></input>
+    <input type='number' placeholder='0'></input>
+  </div>
+    <button>Add Them</button>    
+    </div>
   );
 }
 
