d2e86be65678a21f32a7be66c8843d8ff4baa0f4
Hammadakra <41269248+Hammadakra@users.noreply.github.com>
2020-07-23 11:52:39 -0700

    update

--- my-cal/src/App.js
+++ my-cal/src/App.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [number1,setNumber1] =useState(0);
+  const [number2,setNumber2] =useState(0);
+  const [Total,setTotal] =useState(number2+number1);
+  
+  function add()
+  {
+    setTotal(number1+number2)
+  }
+
+
   return (
     <div>
     <h1>Adding Two Number</h1>
     <div className="inp">
-    <input type='number' placeholder='0'></input>
-    <input type='number' placeholder='0'></input>
+
+    <input 
+    type='number'
+    placeholder='0'
+    value={number1} 
+    onChange={e => setNumber1(e.target.value)}></input>
+    
+    <input 
+    type='number' 
+    placeholder='0' 
+    value={number2}
+    onChange={e => setNumber2(e.target.value)}>
+    </input>
   </div>
-    <button>Add Them</button>    
+  <br></br>
+    <button onClick={add}>Add Them</button>   
+    <h1>{add}</h1> 
     </div>
   );
 }
